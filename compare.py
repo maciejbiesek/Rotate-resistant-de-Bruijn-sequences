f07c1610c78b7e1fb2df5efc22314f79fc3eea0f
Maciej Biesek <maciejbiesek@gmail.com>
2016-06-28 14:35:00 +0200

    editing comparison file

--- compare.py
+++ compare.py
@@ -1,12 +1,13 @@
-import de_bruijn_sequence as db
 import rotate_resistant_de_bruijn_sequence as dbrr
 
-print db.de_bruijn(2, 3)
-print dbrr.de_bruijn_rotate_resistant(2, 3)
+f = open("output", "a")
+for k in range(2, 11):
+    for n in range(2, 10):
+        de_bruijn_rr = dbrr.de_bruijn_rotate_resistant(k, n)
+        lenght = len(de_bruijn_rr)
+        var = k ** (n-1) + n - 1
+        print "k = %d, n = %d, sequence: %s, length = %d, k ** (n-1) + n - 1 = %d, result = %s" % (k, n, de_bruijn_rr, lenght, var, lenght == var)
+		
+	f.write("k = %d, n = %d, sequence: %s, length = %d, k ** (n-1) + n - 1 = %d, result = %s \n" % (k, n, de_bruijn_rr, lenght, var, lenght == var))
 
-k = 4
-for n in range(2, 10):
-    de_bruijn_rr = dbrr.de_bruijn_rotate_resistant(k, n)
-    lenght = len(de_bruijn_rr)
-    var = k ** (n-1) + n - 1
-    print "n = %d, length = %d, k ** (n-1) + n - 1 = %d, result = %s" % (n, lenght, var, lenght == var)
+f.close()
